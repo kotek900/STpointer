515b2fbf6a98d65712e93084b30cee0c222344b4
kotek900 <43883812+kotek900@users.noreply.github.com>
2026-02-27 18:54:12 +0100

    fix some bugs

--- STpointer.h
+++ STpointer.h
@@ -21,6 +21,7 @@ const bool NEW = true;
 #endif
 
 #include <vector>
+#include <iostream>
 
 template <typename T>
 class STpointer;
@@ -32,10 +33,12 @@ private:
 	STnode(const STnode&) = delete;
 
 	void addReferenceToThis(STpointer<STnode>* pointer) {
+		std::cout << "ADD\n";
 		this->referencesToThis.push_back(pointer);
 	}
 
 	void removeReferenceToThis(STpointer<STnode>* pointer) {
+		std::cout << "REMOVE\n";
 		for (int i = 0; i < this->referencesToThis.size(); i++) {
 			if (this->referencesToThis[i] != pointer) continue;
 			this->referencesToThis.erase(this->referencesToThis.begin() + i);
@@ -74,13 +77,20 @@ private:
 	// returns true if loop should be deleted
 	bool unloop(void * stop) {
 		if (parent == stop) return true;
+		if (parent == nullptr) return false;
 		for (int i = 1; i < pointer->referencesToThis.size(); i++) {
 			if (!pointer->referencesToThis[i]->loopsTo(stop)) {
 				std::swap(pointer->referencesToThis[i], pointer->referencesToThis[0]);
 				return false;
 			}
 		}
-		return pointer->referencesToThis[0]->unloop(stop);
+		return parent->referencesToThis[0]->unloop(stop);
+	}
+
+	void setPointer(STnode * pointer) {
+		if (pointer) pointer->addReferenceToThis((STpointer<STnode>*)this);
+		if (this->pointer) this->unset();
+		this->pointer = pointer;
 	}
 
 	template<typename U>
@@ -96,22 +106,22 @@ public:
 	STpointer(bool create, Args... args) {
 		if (create == false) pointer = nullptr;
 		else {
-			pointer = new T(args...);
-			pointer->addReferenceToThis((STpointer<STnode>*)this);
+			this->pointer = nullptr;
+			setPointer(new T(args...));
 		}
 		parent = nullptr;
 	}
 	#endif
 
 	STpointer(STnode* value) {
-		pointer = value;
-		pointer->addReferenceToThis((STpointer<STnode>*)this);
+		this->pointer = nullptr;
+		setPointer(value);
 		parent = nullptr;
 	}
 
 	STpointer(const STpointer& other) {
-		this->pointer = other.pointer;
-		this->pointer->addReferenceToThis((STpointer<STnode>*)this);
+		this->pointer = nullptr;
+		setPointer(other->pointer);
 		this->parent = nullptr;
 	}
 
@@ -127,26 +137,11 @@ public:
 			if (parent->referencesToThis[0]->unloop(parent)) delete pointer;
 			return *this;
 		}
-		if (this->pointer == other.pointer) return *this;
-		if (other.pointer == nullptr) {
-			this->pointer->removeReferenceToThis((STpointer<STnode>*)this);
-		} else if (this->pointer) {
-			bool tree = this->isTree();
-			this->pointer = other.pointer;
-			this->pointer->removeReferenceToThis((STpointer<STnode>*)this);
-			this->pointer->addReferenceToThis((STpointer<STnode>*)this);
-			if (this->pointer->referencesToThis.size() == 0) {
-				delete pointer;
-				return *this;
-			}
-			if (!parent->referencesToThis[0]->loopsTo(parent)) return *this;
-			if (parent->referencesToThis[0]->unloop(parent)) delete pointer;
-		}
+		setPointer(other.pointer);
 		return *this;
 	}
 
 	~STpointer() {
-		if (this->pointer == nullptr) return;
 		this->unset();
 	}
 
